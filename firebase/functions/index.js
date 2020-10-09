88f260575ff8ffe6a5ce3273c9d399e80eb1cc5a
yzia2000 <e0381439@u.nus.edu>
2020-10-09 23:48:57 +0800

    added Activity handler and other requests

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -2,6 +2,8 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
+const {user} = require("firebase-functions/lib/providers/auth");
+const ActivityHandler = require("./utils/ActivityHandler");
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -12,36 +14,112 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  return res.status(200).send("Hello World!");
+    return res.status(200).send("Hello World!");
 });
 
 app.post("/write-doc", async (req, res) => {
-  const { collection, documentId, documentValue } = req.body;
-  const docRef = db.collection(collection).doc(documentId);
-  try {
-    const r = await docRef.set(documentValue);
-    console.log("Write Success", r);
-    return res.status(200).end();
-  } catch (e) {
-    console.error("Write Failure", r);
-    return res.status(500).end();
-  }
+    const { collection, documentId, documentValue } = req.body;
+    const docRef = db.collection(collection).doc(documentId);
+    try {
+        const r = await docRef.set(documentValue);
+        console.log("Write Success", r);
+        return res.status(200).end();
+    } catch (e) {
+        console.error("Write Failure", r);
+        return res.status(500).end();
+    }
 });
 
 app.get("/read-doc", async (req, res) => {
-  const { collection, documentId } = req.query;
-  const docRef = db.collection(collection).doc(documentId);
-  try {
-    const r = await docRef.get();
-    console.log("Read Success", r);
-    return res.status(200).send({
-      name: r.id,
-      value: r.data(),
-    });
-  } catch (e) {
-    console.error("Read Failure", e);
-    return res.status(500).end();
-  }
+    const { collection, documentId } = req.query;
+    const docRef = db.collection(collection).doc(documentId);
+    try {
+        const r = await docRef.get();
+        console.log("Read Success", r);
+        return res.status(200).send({
+            name: r.id,
+            value: r.data(),
+        });
+    } catch (e) {
+        console.error("Read Failure", e);
+        return res.status(500).end();
+    }
 });
+
+app.get("/get-activity", async (req, res) => {
+    collection = "activities";
+    const { documentId } = req.query;
+    const docRef = db.collection(collection).doc(documentId);
+    try {
+        const r = await docRef.get();
+        console.log("Read Success", r);
+        return res.status(200).send({
+            name: r.id,
+            value: r.data(),
+        });
+    } catch (e) {
+        console.error("Read Failure", e);
+        return res.status(500).end();
+    }
+});
+
+app.get("/get-all-activities", async (req, res) => {
+    try {
+        const data = await ActivityHandler.getAllActivities();
+        return res.status(200).send(data);
+    } catch (e) {
+        console.error("Read Failure", e);
+        return res.status(500).end();
+    }
+});
+
+app.post("/create-activity", async (req, res) => {
+    const { userId, activity } = req.query;
+    try {
+        createActivity(userId, activity);
+        console.log("Write Success");
+        return res.status(200).end();
+    } catch (e) {
+        console.error("Read Failure", e);
+        return res.status(500).end();
+    }
+});
+
+app.post("/edit-activity", async (req, res) => {
+    const { activityId, newActivity } = req.query;
+    try {
+        editActivity(activityId, newActivity);
+        console.log("Write Success");
+        return res.status(200).end();
+    } catch (e) {
+        console.error("Read Failure", e);
+        return res.status(500).end();
+    }
+});
+
+app.post("/join-activity", async (req, res) => {
+    const { userId, activityId } = req.query;
+    try {
+        joinActivity(userId, activityId);
+        console.log("Write Success");
+        return res.status(200).end();
+    } catch (e) {
+        console.error("Read Failure", e);
+        return res.status(500).end();
+    }
+});
+
+app.post("/leave-activity", async (req, res) => {
+    const { userId, activityId } = req.query;
+    try {
+        leaveActivity(userId, activityId);
+        console.log("Write Success");
+        return res.status(200).end();
+    } catch (e) {
+        console.error("Read Failure", e);
+        return res.status(500).end();
+    }
+});
+
 
 exports.api = functions.https.onRequest(app);
---
+++ firebase/functions/utils/ActivityHandler.js
@@ -0,0 +1,78 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const ActivityHandler = {
+    getAllActivities : async function() {
+        const db = admin.firestore();
+        const collRef = db.collection("activities");
+        const r = await collRef.get();
+        const data = [];
+        r.forEach(doc => {
+            const activity = doc.data();
+            activity["id"] = doc.id;
+            data.push(activity);
+        });
+        return data;
+    },
+
+    createActivity : function(userId, activity) {
+        const db = admin.firestore();
+        verifyAdmin(userId).then(result => {
+            if (!result) {
+                return;
+            }
+            console.log(activity);
+            const doc = db.collection("activities").add(activity);
+            console.log(doc.id);
+        });
+    },
+
+    verifyAdmin : async function(userId) {
+        const db = admin.firestore();
+        doc = await db.collection("users").doc(userId).get();
+        console.log(doc.data());
+        if (doc.data().type == "Admin") {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    editActivity : function(activityId, newActivity) {
+        const db = admin.firestore();
+        verifyAdmin(userId).then(result => {
+            if (!result) {
+                return
+            }
+            db.collection("activities").doc(activityId).update(newActivity);
+        });
+    },
+
+    joinActivity : function(userId, activityId) {
+        const db = admin.firestore();
+        db.collection("activities").doc(activityId).get().then(doc => {
+            if (doc.data().participants.includes(userId)) {
+                throw "Already signed up";
+            } else {
+                db.collection("activities").doc(activityId).update({
+                    participants : admin.firestore.FieldValue.arrayUnion(userId)
+                });
+            }
+        });
+    },
+
+    leaveActivity: function(userId, activityId) {
+        const db = admin.firestore();
+        db.collection("activities").doc(activityId).update({
+            participants : admin.firestore.FieldValue.arrayRemove(userId)
+        });
+    },
+
+    getActivity : async function(activityId) {
+        const db = admin.firestore();
+        let doc = await db.collection("activities").doc(activityId).get();
+        return doc.data();
+    }
+}
+
+module.exports = ActivityHandler;
