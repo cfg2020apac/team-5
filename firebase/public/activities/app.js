64d0596097803b0e46af63a3db0a9efdf55d75ec
yzia2000 <e0381439@u.nus.edu>
2020-10-09 18:36:11 +0800

    createActivity function

--- firebase/public/activities/app.js
+++ firebase/public/activities/app.js
@@ -6,4 +6,32 @@ async function init() {
     });
 }
 
+function createActivity(name, description, mediaArray, startTime, endTime, description, tags, location) {
+    const db = firebase.firestore();
+
+    let isAdmin;
+    db.collection("users").doc(userId).get().then((doc) => {
+        console.log(doc.data());
+        if (doc.data().type == "Admin") {
+            isAdmin = true;
+        } else {
+            isAdmin = false;
+        }
+    });
+
+    const activity = {
+        name : name,
+        description : description,
+        startBy : startTime,
+        endBy : endTime,
+        createdBy : userId,
+        mediaArray : mediaArray,
+        tags : tags,
+        location : location
+    };
+
+    console.log(activity);
+    db.collection("activities").add(activity);
+}
+
 init();
