6ef9a5a59aa24104c9f22a04def912c4519f7d24
yzia2000 <e0381439@u.nus.edu>
2020-10-10 03:15:28 +0800

    changed viewSignups

--- firebase/public/activities/viewActivity.js
+++ firebase/public/activities/viewActivity.js
@@ -1,19 +1,17 @@
-var activity = {
-    "name": "Something",
-    "createdBy": "asdklfja;skdjfu9841234",
-    "image": "https://picsum.photos/200/200",
-    "startBy": "12-2-2020",
-    "endBy": "13-2-2020",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "mediaArray": ["asdfadf;", "asdfkasdflkj"],
-    "tags": ["tag1", "tag2"],
-    "location": "12 Ang Mo "
-}
-
 activityId = "Zdb7Vtd9OvDwXwgqZBn4";
+userId = "WdEP7GsDztjOTlUFhpaj";
 
 window.onload = async(event) => {
     activity = await getActivity(activityId);
+    let activityStatus = false;
+    console.log(activity.participants);
+
+    if (activity.participants.includes(userId)) {
+        activityStatus = true;
+        document.getElementById("sign-up-btn").classList.add("btn-danger");
+        document.getElementById("sign-up-btn").innerHTML = "Unregister"
+    }
+
     console.log(activity);
     document.getElementById("activity_image").src = activity.image;
     document.getElementById("activity_name").innerHTML = activity.name;
@@ -22,12 +20,19 @@ window.onload = async(event) => {
     document.getElementById("activity_location").innerHTML = activity.location;
     document.getElementById("activity_description").innerHTML = activity.description;
     var divContainer = document.getElementById("container-tags");
-
     for (i in activity.tags) {
         divContainer.append(activity.tags[i]);
     }
+    document.getElementById("sign-up-btn").addEventListener("click", (e) => {
+        if (activityStatus) {
+            document.getElementById("sign-up-btn").classList.remove("btn-danger");
+            document.getElementById("sign-up-btn").innerHTML = "Sign Up";
+            leaveActivity(userId, activityId);
+        } else {
+            document.getElementById("sign-up-btn").classList.add("btn-danger");
+            document.getElementById("sign-up-btn").innerHTML = "Unregister"
+            joinActivity(userId, activityId);
+        }
+        activityStatus = !activityStatus;
+    });
 };
-
-function signup() {
-    joinActivity("WdEP7GsDztjOTlUFhpaj", activityId);
-}
